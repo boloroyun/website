f2f0180f4906a4f0c15bf0b1968038dcede54d5f
BOLOR OYUN TSEVEENJALBA <63110575+boloroyun@users.noreply.github.com>
2025-09-09 01:43:52 -0400

    Fix useCrispTriggers import in CrispManager.tsx

--- components/CrispManager.tsx
+++ components/CrispManager.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useCrispTriggers } from '@/hooks/useCrispTriggers';
+import useCrispTriggers from '@/hooks/useCrispTriggers';
 import CrispIdentifyFromSession from './CrispIdentifyFromSession';
 
 /**
